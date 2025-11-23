0f831b3d1de3d9ff2e4685ef387747147fcb55db
Natalya-Kulkova <Nat4shaky@yandex.ru>
2025-11-23 13:38:23 +0300

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+     //   while(true) {
+     //       System.out.println("Цикл");
+     //   }
+        //Прямой счёт
+        int i = 1;
+        while (i < 10) {
+            System.out.println("Итерация - " + i);
+            i++;
+        }
+    }
+}
