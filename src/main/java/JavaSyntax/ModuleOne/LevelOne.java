76d63bd54512004342edf41ed77a4a898db64e19
Natalya Kulkova <Adissinia@gmail.com>
2025-12-22 15:51:09 +0300

    Rebase

---
+++ src/main/java/JavaSyntax/ModuleOne/LevelOne.java
@@ -0,0 +1,96 @@
+package JavaSyntax.ModuleOne;
+
+public class LevelOne {
+    public static void main(String[] args){
+    //1.Напиши программу, которая выводит на экран надпись: "Амиго очень умный".
+       // System.out.println("Амиго очень умный");//напишите тут ваш код
+
+//2.Напиши программу, которая в методе main объявляет такие переменные:
+//name типа String, age типа int и city типа String.
+        //String name;
+        //int age;
+        //String city;
+
+//3.Объяви переменные a, b типа int.
+//Сразу же в строке объявления присвой им разные значения.
+//Значениями могут быть любые целые числа.
+//        int a = 25;
+//        int b = 52;
+
+//4. Сейчас 3126 год. Мой друг родился 8 лет назад.
+//Написать программу, выводящую на экран год рождения моего друга.
+//        int nowYear = 3126;
+//        System.out.println(nowYear-8);
+
+//5. В методе main объяви переменные word, phrase, line и text типа String.
+//Присвой им разные значения. Используй одну команду для создания и инициализации переменных.
+//Значениями могут быть любые строки.
+//        String word = "Java";
+//        String phrase = "I learn Java";
+//        String line = "This line of Java";
+//        String text = "Another text about Java";
+
+//6. В переменную tagline нужно записать строку "JustDoIt".
+//Используй s1, s2, s3 и конкатенацию строк
+//        String s1 = "Just";
+//        String s2 = "Do";
+//        String s3 = "It";
+//        String tagLine = s1 + s2 + s3;
+//        System.out.print(tagLine);
+
+//7. Метод main выводит на экран четыре строки. Все они - яркий пример злоупотребления заглавными буквами.
+//Внеси изменения в код, чтобы все буквы в этих строках стали строчными.
+//        String title = "Senior Lead Principal Software Engineer Data Architect";
+//        String degree = "In college, I Majored in Political Science and Minored in Religious Studies.";
+//        String career = "Experienced Team Leader with strong Organizational Skills and a Successful career in Management.";
+//
+//        System.out.println("RESUME".toLowerCase());
+//        System.out.println("TITLE: ".toLowerCase() + title.toLowerCase());
+//        System.out.println("DEGREE: ".toLowerCase() + degree.toLowerCase());
+//        System.out.println("CAREER: ".toLowerCase() + career.toLowerCase());
+
+
+//8. В методе main на экран выводятся три строки.
+//Внеси изменения в код, чтобы все буквы в этих строках стали заглавными.
+//        String caps = "if I type in caps ";
+//        String usa = "сша";
+//
+//        System.out.println("nata".toUpperCase());
+//        System.out.println(usa.toUpperCase());
+//        System.out.println((caps + "they know what i mean").toUpperCase());
+
+//9. В методе main объяви переменные intValue, numericValue, size, number типа int.
+// Присвой им разные значения. Используй одну команду для создания и инициализации переменных.
+// Значениями могут быть любые целые числа.
+//       int IntValue = 2, numericValue = 67, size = 123, number = 456;
+
+
+//10. В переменной centimeters записано количество сантиметров. В переменную meters нужно записать количество полных метров в centimeters.
+// (1 метр = 100 см). Для вычисления используй переменную centimeters и оператор деления.
+// Для объявления и инициализации meters используй одну команду.
+//        int centimeters = 123;
+//        int meters = centimeters/100;
+
+//11. В переменной number записано число. В переменную lastDigit нужно записать последнюю цифру этого числа.
+// Для вычисления используй переменную number и оператор «остаток от деления».
+// Для объявления и инициализации lastDigit используй одну команду. Подсказка: делить нужно на 10.
+//        int number = 26;
+//        int lastDigit = number % 10;
+
+//12. Используя только оператор декремента, измени значение переменной toothCounter, чтобы на экран вывелось число 23.
+        int toothCounter = 32;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+
+
+
+    }
+
+}
