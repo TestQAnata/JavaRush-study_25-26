003e6fe9ca2c8402373eaecdae3340d4fb3da43b
Natalya Kulkova <Adissinia@gmail.com>
2025-12-24 17:41:50 +0300

    Уровни 3-5

---
+++ src/main/java/JavaSyntax/ModuleOne/LevelFive.java
@@ -0,0 +1,47 @@
+package JavaSyntax.ModuleOne;
+
+import java.util.Scanner;
+
+public class LevelFive {
+    public static void main(String[] args) {
+//1. Используя цикл while вывести на экран сто раз цитату (переменная quote):
+// «Я никогда не буду работать за копейки. Амиго» Каждое значение вывести с новой строки.
+//        String quote = "Я никогда не буду работать за копейки. Амиго";
+//        int b = 0;
+//                while (b < 99) {
+//                    System.out.println(quote);
+//                    b++;
+//                }
+
+//2.Напишем программу, в которой нужно вводить с клавиатуры целые числа и считать их сумму,
+// пока пользователь не введет слово "ENTER". Вывести на экран полученную сумму и завершить программу.
+//        Scanner console = new Scanner(System.in);
+//        boolean isExit = false;
+//        int sum = 0;
+//        while (!isExit) {
+//            if (console.hasNextInt()){
+//                sum += console.nextInt();
+//                console.nextLine();
+//            } else {
+//                isExit = console.nextLine().equals("ENTER");
+//            }
+//        }
+//        System.out.println(sum);
+
+//3.  Используя вложенные циклы while (цикл в цикле) выведи на экран прямоугольник
+// размером 5 (высота) на 10 (ширина), заполненный буквой 'Q'.
+//        int n = 0;
+//        while (n < 5){ int m = 0;
+//            while (m < 10){
+//                System.out.print("Q");
+//                m++;
+//            }
+//            System.out.println("Q");
+//            n++;
+//        }
+
+//4. Используя вложенные циклы while (цикл в цикле) выведи на экран незаполненный прямоугольник
+// (его контур) размером 10 (высота) на 20 (ширина) из букв 'Б'. Незаполненная часть состоит из пробелов.
+
+    }
+}
