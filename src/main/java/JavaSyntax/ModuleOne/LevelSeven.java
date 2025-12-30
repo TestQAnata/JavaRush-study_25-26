6d453011d7342b7419931df73acc8eac1c98c9ae
Natalya Kulkova <Adissinia@gmail.com>
2025-12-30 10:53:48 +0300

    Уровень 7 - задачи 1-2

---
+++ src/main/java/JavaSyntax/ModuleOne/LevelSeven.java
@@ -0,0 +1,57 @@
+package JavaSyntax.ModuleOne;
+
+import java.util.Scanner;
+
+public class LevelSeven {
+    public static int[][] multiArray;
+    public static void main(String[] args) {
+//1. Создай треугольный массив, где значение каждого элемента — это сумма его индексов.
+//        int[][] array = new int[10][];
+//        array[0] = new int[1];
+//        array[1] = new int[2];
+//        array[2] = new int[3];
+//        array[3] = new int[4];
+//        array[4] = new int[5];
+//        array[5] = new int[6];
+//        array[6] = new int[7];
+//        array[7] = new int[8];
+//        array[8] = new int[9];
+//        array[9] = new int[10];
+//
+//        for (int i = 0; i < array.length; i++) {
+//            for (int j = 0; j < array[i].length; j++) {
+//                array[i][j] = i + j;
+//            }
+//        }
+//        for (int i = 0; i < array.length; i++) {
+//            for (int j = 0; j < array[i].length; j++) {
+//
+//                System.out.print(array[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
+
+ //2. Создай двумерный массив. Для этого реализуй метод main(String[]), который:
+        //Считывает из консоли число N — количество строк массива
+        // (считай, что это число будет больше 0, можно не проверять).
+        //Считывает N чисел из консоли (любые числа, которые будут больше 0, можно не проверять).
+        //Инициализирует двумерный массив multiArray:
+        //количеством строк N;
+        //строки — массивами, размер которых соответствует числам, введенным в п.2 (в порядке ввода).
+//        Scanner scanner = new Scanner(System.in);
+//        int N = scanner.nextInt();
+//        multiArray = new int[N][];
+//        for (int i = 0; i < multiArray.length; i++) {
+//            int size = scanner.nextInt();
+//            multiArray[i] = new int[size];
+//        }
+
+
+//3. В методе main(String[]) выведи на экран все числа из трехмерного массива multiArray.
+//Требования:
+//•	В классе Solution должна быть публичная статическая переменная multiArray типа int[][][].
+//•	В методе main(String[]) выведи на экран все числа из multiArray.
+
+    }
+}
+
