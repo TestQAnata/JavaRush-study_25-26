928e7887e808a08df7cd6dcecef3f92a5674dcc1
Наталья <testqanata@gmail.com>
2026-01-04 14:13:29 +0300

    Уровень 8 - продолжение

--- src/main/java/JavaSyntax/ModuleOne/LevelEight.java
+++ src/main/java/JavaSyntax/ModuleOne/LevelEight.java
@@ -1,5 +1,7 @@
 package JavaSyntax.ModuleOne;
 
+import java.util.Scanner;
+
 public class LevelEight {
     public static void main(String[] args) {
 
@@ -154,5 +156,92 @@ public class LevelEight {
         //
         //    }
 
-    }
-}
+ //7. Перед тобой метод signIn(), который приветствует пользователей сайта. Сейчас он приветствует всех пользователей, а должен только зарегистрированных. Имена всех незарегистрированных пользователей — "user".
+        //Добавь проверку имени пользователя в начало метода signIn().
+        //Если имя "user", прерви выполнение метода, используя ключевое слово return
+//        public static void signIn(String username){
+//            if (username.equals("user")) {
+//                return;
+//            }
+//            //напишите тут ваш код
+//            System.out.println("Добро пожаловать " + username);
+//            System.out.println("Очень скучали по Вам, " + username);
+//        }
+
+//8. Давай напишем реализацию калькулятора, который будет считать степени чисел.
+//Для этого создай метод cube(). В качестве аргумента он должен принимать целочисленное значение типа long.
+//Метод должен возводить полученное значение в третью степень и возвращать его как результат работы метода.
+//Числа, которыми придется оперировать, могут быть большими. Поэтому метод cube() должен возвращать тип long.
+
+//    }
+//        public static long cube(long num){
+//            long result = num * num * num;
+//            return result;
+//
+//        }
+
+//9. Перед тобой программа, которая выводит информацию о человеке в консоли. К сожалению, она не компилируется.
+//Измени минимальное необходимое количество модификаторов доступа в классе Person, чтобы код скомпилировался.
+//        public class Person {
+//            private String firstName;
+//            private String lastName;
+//
+//            public Person(String firstName, String lastName) {
+//                this.firstName = firstName;
+//                this.lastName = lastName;
+//            }
+//
+//            protected String getFirstName() {
+//                return firstName;
+//            }
+//
+//            String getLastName() {
+//                return lastName;
+//            }
+//
+//            public String getFullName(){
+//                return firstName + " " + lastName;
+//            }
+//        }
+
+//10.Данная программа должна выводить информацию о созданном массиве. Но из-за неправильно расставленных модификаторов static она не компилируется.
+//Исправь эти ошибки. Добавь модификатор static там, где это необходимо.
+//        public class Solution {
+//            public static void main(String[] args) {
+//                int[] array = {123, 7, -14, 67, 16, 98, 75, 576};
+//                System.out.println("В массиве: ");
+//                printArray(array);
+//                System.out.println("Минимальное число: " + getMinValue(array));
+//                System.out.println("Максимальное число: " + getMaxValue(array));
+//
+//            }
+//
+//            public static int getMaxValue(int[] array) {
+//                int max = Integer.MIN_VALUE;
+//                for (int value : array) {
+//                    if (value > max) {
+//                        max = value;
+//                    }
+//                }
+//                return max;
+//            }
+//
+//            public static int getMinValue(int[] array) {
+//                int min = Integer.MAX_VALUE;
+//                for (int value : array) {
+//                    if (value < min) {
+//                        min = value;
+//                    }
+//                }
+//                return min;
+//            }
+//
+//            public static void printArray(int[] array) {
+//                for (int i : array) {
+//                    System.out.print(i + " ");
+//                }
+//                System.out.println();
+//            }
+//        }
+
+//11.
