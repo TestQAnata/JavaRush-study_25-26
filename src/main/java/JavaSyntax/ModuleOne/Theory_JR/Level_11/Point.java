9713278fdab9ef14f30a39eaa962de372fdacf35
Natalya Kulkova <Adissinia@gmail.com>
2026-02-04 12:17:01 +0300

    11

---
+++ src/main/java/JavaSyntax/ModuleOne/Theory_JR/Level_11/Point.java
@@ -0,0 +1,8 @@
+package JavaSyntax.ModuleOne.Theory_JR.Level_11;
+
+public class Point {
+    public int x;
+    public int y;
+    public int color;
+
+}
---
+++ src/main/java/JavaSyntax/ModuleOne/Theory_JR/Level_11/Solution.java
@@ -0,0 +1,18 @@
+package JavaSyntax.ModuleOne.Theory_JR.Level_11;
+
+public class Solution {
+    public static void printPoint(Point[] points) {
+        for (int i = 0; i < points.length; i++) {
+            System.out.println("Цвет точки х=" + points[i].x + ", y=" + points[i].y + " " + points[i].color);
+        }
+        
+        public static void main(String[] args)
+        {
+            Point[] data = new Point[100];
+            for (int i = 0; i < ; i++) {
+                
+            }
+        }
+                
+    }
+}
